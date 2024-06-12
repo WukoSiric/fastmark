e7b9e118cc6eb8ca2822f65c9c790d067547c191
WukoSiric <lstainer2002@gmail.com>
2024-06-12 17:05:49 +0930

    Client side markdown previews

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,12 @@
 <script>
+import { marked } from 'marked'
+
 export default {
   data() {
     return {
       content: '',
+      parsed: '',
+      userInput: '',
     }
   },
   methods: {
@@ -14,11 +18,22 @@ export default {
         this.content = 'Error'
       }
     },
+    updatePreview() {
+      this.parsed = marked(this.userInput)
+    },
   },
 }
 </script>
 
 <template>
+  <div class="grid grid-cols-2">
+    <textarea
+      v-model="userInput"
+      @input="updatePreview"
+      class="h-48 bg-neutral-600 p-2 text-white"
+    ></textarea>
+    <div class="prose" v-html="parsed"></div>
+  </div>
   <div class="grid h-screen place-content-center gap-3">
     <h1 class="flex text-5xl font-bold">Hello world</h1>
     <button @click="sendRequest" class="rounded-lg bg-blue-500 p-2 font-medium text-amber-100">
