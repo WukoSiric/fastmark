157edae991242686863f91fd5917d25e910d492d
WukoSiric <lstainer2002@gmail.com>
2024-06-13 20:30:49 +0930

    chore: Update fonts and colors in client code

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+      colors: {
+        brand: {
+          main: '#448CC2',
+          accent: '#62B2E9',
+        },
+        background: {
+          main: 'var(--main-bg)',
+          sidebar: 'hsl(var(--sidebar-bg) / <alpha-value>)',
+        },
+        txt: {
+          editor: 'hsl(var(--editor-txt) / <alpha-value>)',
+          detail: 'hsl(var(--detail-txt) / <alpha-value>)',
+          note: 'hsl(var(--note-txt) / <alpha-value>)',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
